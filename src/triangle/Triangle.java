ccb5b3529ffebee9681cdaa9620575fe452ff7f4
Max <max.ferchland@gmx.de>
2025-06-11 13:52:44 +0200

    Aufgabe 1 und anfang aufgabe 2: noch out of memory exception bei whileshleife

--- src/triangle/Triangle.java
+++ src/triangle/Triangle.java
@@ -32,12 +32,37 @@ public class Triangle implements ResizableImage {
         BufferedImage bufferedImage = new BufferedImage(size.width, size.height, BufferedImage.TYPE_INT_ARGB);
         Graphics2D gBuffer = (Graphics2D) bufferedImage.getGraphics();
         gBuffer.setColor(Color.black);
-        int border = 2;
+        /*int border = 2;
         gBuffer.drawRect(border, border, size.width - 2 * border, size.height - 2 * border);
         gBuffer.setColor(Color.darkGray);
         border = 8;
         gBuffer.drawRect(border, border, size.width - 2 * border, size.height - 2 * border);
-        gBuffer.drawString("Triangle goes here", border * 2, border * 4);
+        gBuffer.drawString("Triangle goes here", border * 2, border * 4);*/
+        int [] x = {400, 112, 688};
+        int [] y = {100, 600, 600};
+        int numPoints = 3;
+        int runningIteration = 0;
+        gBuffer.drawPolygon(x, y, numPoints);
+        while(runningIteration <1){
+            int x1 = (x[0]+x[1])/2;
+            int x2 = x[0];
+            int x3 = (x[1]+x[2])/2;
+            int y1 = (y[0]+y[1])/2;
+            int y2 = y[0];
+            int y3 = (y[1]+y[2])/2;
+            x[0] = x1;
+            x[1] = x2;
+            x[2] = x3;
+            y[0] = y1;
+            y[1] = y2;
+            y[2] = y3;
+            runningIteration++;
+            drawTriangle(size);
+
+        }
+        int [] testx = {256, 400, 544};
+        int [] testy = {350, 600, 350};
+        gBuffer.drawPolygon(testx, testy, numPoints);
         return bufferedImage;
     }
 
